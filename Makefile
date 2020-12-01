06a48534f718333474658f3990753f639efcc7e4
Mark Nelson <marknels@wiliki.eng.hawaii.edu>
2020-12-01 10:29:51 -1000

    Initial commit.

---
+++ Makefile
@@ -0,0 +1,5 @@
+all: empire
+
+empire: main.cpp
+	g++ -std=c++17 -o empire main.cpp
+
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Welcome to Empire\n";
+	return 0;
+}
+
