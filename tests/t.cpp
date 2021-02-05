b6ca1012c0923ca75d1ea918907d724f67515fd1
Mark Nelson <marknels@hawaii.edu>
2021-02-05 13:22:36 -1000

    First working version of Boost UTF - Unit Test Framework

--- tests/t.cpp
+++ tests/t.cpp
@@ -1,11 +1,17 @@
-#include "../Commodities/Commodity.hpp"
-
-#define BOOST_TEST_MODULE MyTest
+#define BOOST_TEST_MODULE Empire_Server
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE( my_test ) {
+#include "../Commodities/Commodity.hpp"
+
+using namespace empire;
+
+BOOST_AUTO_TEST_SUITE( Commodities )
+
+
+BOOST_AUTO_TEST_CASE( Commodity_01 ) {
     Commodity testCommodity( true, 100 );
 
-    BOOST_CHECK( testCommodity.validate() );
+    BOOST_REQUIRE_NO_THROW( testCommodity.validate() );
 }
 
+BOOST_AUTO_TEST_SUITE_END()
