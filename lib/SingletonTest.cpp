459b8d033a48c69300fff8e98b130789fda34997
Mark Nelson <marknels@hawaii.edu>
2021-03-20 17:04:12 -1000

    Add a 2nd Singleton test class

--- lib/SingletonTest.cpp
+++ lib/SingletonTest.cpp
@@ -24,29 +24,55 @@
 using namespace empire;
 
 
-class TestSingle final : public Singleton<TestSingle> {
+class TestSingleton1 final : public Singleton<TestSingleton1> {
 public:
-	TestSingle(token) ;
-	~TestSingle();
+	TestSingleton1(token) ;
+	~TestSingleton1();
 	
 	void use() const;
 		
-}; // class TestSingle
+}; // class TestSingleton1
 
-
-TestSingle::TestSingle(token) {
-	// cout << "TestSingle constructed" << endl;
+TestSingleton1::TestSingleton1(token) {
+	// cout << "TestSingleton1 constructed" << endl;
 }
 
 
-TestSingle::~TestSingle() {
-	// cout << "TestSingle destroyed" << endl;
+TestSingleton1::~TestSingleton1() {
+	// cout << "TestSingleton1 destroyed" << endl;
 }
 
 	
-void TestSingle::use() const {
-	// cout << "TestSingle in use" << endl;
+void TestSingleton1::use() const {
+	// cout << "TestSingleton1 in use" << endl;
 }
+
+
+/// Create a 2nd test class... to verify that we can have
+/// multiple dervied classes of Singleton
+class TestSingleton2 final : public Singleton<TestSingleton2> {
+public:
+	TestSingleton2(token) ;
+	~TestSingleton2();
+	
+	void use() const;
+		
+}; // class TestSingleton2
+
+TestSingleton2::TestSingleton2(token) {
+	// cout << "TestSingleton1 constructed" << endl;
+}
+
+
+TestSingleton2::~TestSingleton2() {
+	// cout << "TestSingleton1 destroyed" << endl;
+}
+
+	
+void TestSingleton2::use() const {
+	// cout << "TestSingleton2 in use" << endl;
+}
+
 
 
 
@@ -57,8 +83,11 @@ BOOST_AUTO_TEST_SUITE( Singleton_test_suite )
 
 /// Test an enabled Commodity constructor
 BOOST_AUTO_TEST_CASE( Singleton_general ) {
-	auto const& t = TestSingle::instance();
-	t.use();
+	auto const& t1 = TestSingleton1::instance();
+	t1.use();
+
+	auto const& t2 = TestSingleton2::instance();
+	t2.use();	
 }
 
 BOOST_AUTO_TEST_SUITE_END()
