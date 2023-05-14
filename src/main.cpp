72b2627c1537a13c467a7f7478c6810335f08083
Mark Nelson <marknels@hawaii.edu>
2023-05-13 16:16:54 -1000

    Add Clang Tidy to the build

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+///////////////////////////////////////////////////////////////////////////////
+//  Empire ][ - What you do is what you do
+//
+/// The Core services of the `Empire ][` server.
+///
+/// @file      main.cpp
+/// @author    Mark Nelson <mr_nelson@icloud.com>
+/// @copyright (c) 2021 Mark Nelson.  All rights reserved.
+///////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+
+/// A sample Hello World program
+///
+/// @return Zero
+   int main() {
+      std::cout << "Hello, World!" << std::endl;
+      return 0;
+   }
