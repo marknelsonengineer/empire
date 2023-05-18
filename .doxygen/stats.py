ca0d55d7b79c6cbd322254d0a7e1f920425d2ed8
Mark Nelson <marknels@hawaii.edu>
2023-05-17 14:33:59 -1000

    Reflow stats.py Knock down Python style errors (PEP) Fixup some formatting issues

--- .doxygen/stats.py
+++ .doxygen/stats.py
@@ -40,8 +40,7 @@ GITHUB_REPO = "empire"
 ## Use this style when printing content from shields_io
 ##
 ## @see https://shields.io
-STYLE = "social"
-
+STYLE = "plastic"
 
 locale.setlocale(locale.LC_ALL, '')  # Use '' for auto
 
@@ -93,12 +92,12 @@ number_of_test_assertions = 0
 ##
 ## @return Nothing
 def print_title():
-	print("Project Statistics")
-	print("==================")
-	print("")
-	print("@brief Document high-level statistics about the source code and")
-	print("       git repository")
-	print("")
+    print("Project Statistics")
+    print("==================")
+    print("")
+    print("@brief Document high-level statistics about the source code and")
+    print("       git repository")
+    print("")
 
 
 ## Print the [Shields.io](https://shields.io) tags
@@ -107,58 +106,66 @@ def print_title():
 ##
 ## @return Nothing
 def print_shields_io_tags():
-	repo1 = f"![GitHub code size	in	bytes](https://img.shields.io/github/languages/code-size/{GITHUB_USERNAME}/{GITHUB_REPO}?style={STYLE})"
-	repo2 = f"![GitHub repo size](https://img.shields.io/github/repo-size/{GITHUB_USERNAME}/{GITHUB_REPO}?style={STYLE})"
-	repo3 = f"![GitHub contributors](https://img.shields.io/github/contributors/{GITHUB_USERNAME}/{GITHUB_REPO}?style={STYLE})"
+    url = "https://img.shields.io/github"
+    brand = f"style={STYLE}"
+    repo1 = f"![X]({url}/languages/code-size/{GITHUB_USERNAME}/{GITHUB_REPO}?{brand})"
+    repo2 = f"![X]({url}/repo-size/{GITHUB_USERNAME}/{GITHUB_REPO}?{brand})"
+    repo3 = f"![X]({url}/contributors/{GITHUB_USERNAME}/{GITHUB_REPO}?{brand})"
 
-	commit1 = f"![GitHub commit activity](https://img.shields.io/github/commit-activity/w/{GITHUB_USERNAME}/{GITHUB_REPO}?style={STYLE})"
-	commit2 = f"![GitHub last	commit](https://img.shields.io/github/last-commit/{GITHUB_USERNAME}/{GITHUB_REPO}?style={STYLE})"
-	issue1 = f"![GitHub	issues](https://img.shields.io/github/issues-raw/{GITHUB_USERNAME}/{GITHUB_REPO}?style={STYLE})"
-	issue2 = f"![GitHub	closed issues](https://img.shields.io/github/issues-closed-raw/{GITHUB_USERNAME}/{GITHUB_REPO}?style={STYLE})"
-	social1 = f"![GitHub forks](https://img.shields.io/github/forks/{GITHUB_USERNAME}/{GITHUB_REPO}?style={STYLE})"
-	social2 = f"![GitHub Repo	stars](https://img.shields.io/github/stars/{GITHUB_USERNAME}/{GITHUB_REPO}?style={STYLE})"
+    commit1 = f"![X]({url}/commit-activity/w/{GITHUB_USERNAME}/{GITHUB_REPO}?{brand})"
+    commit2 = f"![X]({url}/last-commit/{GITHUB_USERNAME}/{GITHUB_REPO}?{brand})"
+    issue1 = f"![X]({url}/issues-raw/{GITHUB_USERNAME}/{GITHUB_REPO}?{brand})"
+    issue2 = f"![X]({url}/issues-closed-raw/{GITHUB_USERNAME}/{GITHUB_REPO}?{brand})"
+    social1 = f"![X]({url}/forks/{GITHUB_USERNAME}/{GITHUB_REPO}?{brand})"
+    social2 = f"![X]({url}/stars/{GITHUB_USERNAME}/{GITHUB_REPO}?{brand})"
 
-	print()
-	print("## GitHub Statistics")
+    print()
+    print("## GitHub Statistics")
 
-	# print_number_of_commits
+    # print_number_of_commits
 
-	print("| Repository                           | Commits                   | Issues                  | Social                    |")
-	print("|--------------------------------------|---------------------------|-------------------------|---------------------------|")
-	print(f"| {repo1} <br/> {repo2} <br/> {repo3} | {commit1} <br/> {commit2} | {issue1} <br/> {issue2} | {social1} <br/> {social2} |")
+    print(
+        "| Repository                           | Commits                   | Issues                  | Social                    |")
+    print(
+        "|--------------------------------------|---------------------------|-------------------------|---------------------------|")
+    print(
+        f"| {repo1} <br/> {repo2} <br/> {repo3} | {commit1} <br/> {commit2} | {issue1} <br/> {issue2} | {social1} <br/> {social2} |")
 
 
 ## Print a table of Git tags (if any)
 ##
 ## @return Nothing
 def print_tags():
-	# Use	the following to get	the tag/release history
-	# $ git tag	--sort=v:refname --format="%(tag)|%(contents)|%(creator)"
-	print("")
-	print("## Tags")
+    # Use	the following to get	the tag/release history
+    # $ git tag	--sort=v:refname --format="%(tag)|%(contents)|%(creator)"
+    print("")
+    print("## Tags")
 
-	tags = repo.tags
+    tags = repo.tags
 
-	if len(tags) == 0:
-		print("No tags")
-	else:
-		print("| Tag | Date | Author |")
-		print("|-----|------|--------|")
+    if len(tags) == 0:
+        print("No tags")
+    else:
+        print("| Tag | Date | Author |")
+        print("|-----|------|--------|")
 
-		# git for-each-ref --sort=creatordate --format '|%(refname:strip=2)|%(creatordate)|%(authorname)|' refs/tags
-		result = subprocess.run(['git', 'for-each-ref', '--sort=creatordate', '--format', '|%(refname:strip=2)|%(creatordate)|%(authorname)|', 'refs/tags'], stdout=subprocess.PIPE)
-		print(result.stdout.decode('utf-8'))
+        # git for-each-ref --sort=creatordate --format '|%(refname:strip=2)|%(creatordate)|%(authorname)|' refs/tags
+        result = subprocess.run(
+            ['git', 'for-each-ref', '--sort=creatordate', '--format',
+             '|%(refname:strip=2)|%(creatordate)|%(authorname)|',
+             'refs/tags'], stdout = subprocess.PIPE)
+        print(result.stdout.decode('utf-8'))
 
-	print("")
+    print("")
 
 
 ## Print `Automatically generated on` and the date
 ##
 ## @return Nothing
 def print_date():
-	print("")
-	now = datetime.datetime.now()
-	print("Automatically generated on " + now.strftime("%c"))
+    print("")
+    now = datetime.datetime.now()
+    print("Automatically generated on " + now.strftime("%c"))
 
 
 ## Find single-line comments (like `//` and `#`) and remove them
@@ -166,12 +173,12 @@ def print_date():
 ## @param line The line with a mix of working source and comments
 ## @param sep The single-line comment delimiter
 ## @return `line` stripped of any comments
-def remove_comments(line, sep="#"):
-	line_str = str(line, 'UTF-8')
-	i = line_str.find(sep)  # Find the position of the separator
-	if i >= 0:
-		line_str = line_str[0:i]
-	return line_str.rstrip()
+def remove_comments(line, sep = "#"):
+    line_str = str(line, 'UTF-8')
+    i = line_str.find(sep)  # Find the position of the separator
+    if i >= 0:
+        line_str = line_str[0:i]
+    return line_str.rstrip()
 
 
 ## Process the set of shell-script files in `file_list` and report their statistics
@@ -185,27 +192,28 @@ def remove_comments(line, sep="#"):
 ## @param file_list The set of files to process
 ## @return Nothing
 def process_script_files(row_name, file_list):
-	if len(file_list) == 0:
-		return
+    if len(file_list) == 0:
+        return
 
-	num_files = len(file_list)
+    num_files = len(file_list)
 
-	total_bytes = 0
-	total_lines = 0
-	total_working_bytes = 0
-	total_working_lines = 0
-	for f in file_list:
-		total_bytes += os.path.getsize(f)
-		total_lines += sum(1 for _ in open(f, "rb"))
-		for line in open(f, "rb"):
-			line = remove_comments(line, "#")
-			if len(line) <= 0:
-				continue
-			total_working_lines += 1
-			total_working_bytes += len(line)
-			# print( line )
+    total_bytes = 0
+    total_lines = 0
+    total_working_bytes = 0
+    total_working_lines = 0
+    for f in file_list:
+        total_bytes += os.path.getsize(f)
+        total_lines += sum(1 for _ in open(f, "rb"))
+        for line in open(f, "rb"):
+            line = remove_comments(line, "#")
+            if len(line) <= 0:
+                continue
+            total_working_lines += 1
+            total_working_bytes += len(line)
+        # print( line )
 
-	print(f"|{row_name}|{num_files:n}|{total_lines:n}|{total_bytes:n}|{total_working_lines:n}|{total_working_bytes:n}|")
+    print(
+        f"|{row_name}|{num_files:n}|{total_lines:n}|{total_bytes:n}|{total_working_lines:n}|{total_working_bytes:n}|")
 
 
 ## Process the set of gcc files in `file_list` and report their statistics
@@ -219,35 +227,36 @@ def process_script_files(row_name, file_list):
 ## @param file_list The set of files to process
 ## @return Nothing
 def process_gcc_files(row_name, file_list):
-	if len(file_list) == 0:
-		return
+    if len(file_list) == 0:
+        return
 
-	num_files = len(file_list)
+    num_files = len(file_list)
 
-	total_bytes = 0
-	total_lines = 0
-	total_working_bytes = 0
-	total_working_lines = 0
-	for f in file_list:
-		total_bytes += os.path.getsize(f)
-		total_lines += sum(1 for _ in open(f, "rb"))
-		result = subprocess.run(['gcc', '-fpreprocessed', '-dD', '-w', '-E', f], stdout=subprocess.PIPE)
+    total_bytes = 0
+    total_lines = 0
+    total_working_bytes = 0
+    total_working_lines = 0
+    for f in file_list:
+        total_bytes += os.path.getsize(f)
+        total_lines += sum(1 for _ in open(f, "rb"))
+        result = subprocess.run(['gcc', '-fpreprocessed', '-dD', '-w', '-E', f], stdout = subprocess.PIPE)
 
-		all_output = ""
-		# lineStr = str(result, 'UTF-8')
-		for char in result.stdout.decode('ascii'):
-			all_output += char
+        all_output = ""
+        # lineStr = str(result, 'UTF-8')
+        for char in result.stdout.decode('ascii'):
+            all_output += char
 
-		for line in all_output.splitlines():
-			line = line.strip()
+        for line in all_output.splitlines():
+            line = line.strip()
 
-			if len(line) <= 0:
-				continue
-			total_working_lines += 1
-			total_working_bytes += len(line)
-			# print( line )
+            if len(line) <= 0:
+                continue
+            total_working_lines += 1
+            total_working_bytes += len(line)
+        # print( line )
 
-	print(f"|{row_name}|{num_files:n}|{total_lines:n}|{total_bytes:n}|{total_working_lines:n}|{total_working_bytes:n}|")
+    print(
+        f"|{row_name}|{num_files:n}|{total_lines:n}|{total_bytes:n}|{total_working_lines:n}|{total_working_bytes:n}|")
 
 
 ## Process the set of text files in `file_list` and report their statistics
@@ -259,18 +268,18 @@ def process_gcc_files(row_name, file_list):
 ## @param file_list The set of files to process
 ## @return Nothing
 def process_text_files(row_name, file_list):
-	if len(file_list) == 0:
-		return
+    if len(file_list) == 0:
+        return
 
-	num_files = len(file_list)
+    num_files = len(file_list)
 
-	total_bytes = 0
-	total_lines = 0
-	for f in file_list:
-		total_bytes += os.path.getsize(f)
-		total_lines += sum(1 for _ in open(f, "rb"))
+    total_bytes = 0
+    total_lines = 0
+    for f in file_list:
+        total_bytes += os.path.getsize(f)
+        total_lines += sum(1 for _ in open(f, "rb"))
 
-	print(f"|{row_name}|{num_files:n}|{total_lines:n}|{total_bytes:n}|n/a|n/a|")
+    print(f"|{row_name}|{num_files:n}|{total_lines:n}|{total_bytes:n}|n/a|n/a|")
 
 
 ## Process the set of data files in `file_list` and report their statistics
@@ -281,16 +290,16 @@ def process_text_files(row_name, file_list):
 ## @param file_list The set of files to process
 ## @return Nothing
 def process_data_files(row_name, file_list):
-	if len(file_list) == 0:
-		return
+    if len(file_list) == 0:
+        return
 
-	num_files = len(file_list)
+    num_files = len(file_list)
 
-	total_bytes = 0
-	for f in file_list:
-		total_bytes += os.path.getsize(f)
+    total_bytes = 0
+    for f in file_list:
+        total_bytes += os.path.getsize(f)
 
-	print(f"|{row_name}|{num_files:n}|n/a|{total_bytes:n}|n/a|n/a|")
+    print(f"|{row_name}|{num_files:n}|n/a|{total_bytes:n}|n/a|n/a|")
 
 
 ## Print the main Project Statistics table.
@@ -301,73 +310,73 @@ def process_data_files(row_name, file_list):
 ##
 ## @return Nothing
 def process_files_in_git():
-	print("| Type | Files | Lines | Bytes | Working Lines | Working Bytes |")
-	print("|------|------:|------:|------:|--------------:|--------------:|")
+    print("| Type | Files | Lines | Bytes | Working Lines | Working Bytes |")
+    print("|------|------:|------:|------:|--------------:|--------------:|")
 
-	global number_of_test_cases
-	global number_of_test_assertions
+    global number_of_test_cases
+    global number_of_test_assertions
 
-	for (_path, _stage), entry in repo.index.entries.items():
-		# print( entry )
+    for (_path, _stage), entry in repo.index.entries.items():
+        # print( entry )
 
-		if not os.path.isfile(_path):
-			continue
+        if not os.path.isfile(_path):
+            continue
 
-		file_extension = pathlib.Path(_path).suffix
-		# print( "Path = " + _path + "    Name = " + "XXXX" + "   Extension = " +	file_extension	)
+        file_extension = pathlib.Path(_path).suffix
+        # print( "Path = " + _path + "    Name = " + "XXXX" + "   Extension = " +	file_extension	)
 
-		# If it's a C++ unit test, then count the test cases and test points
-		if _path[0:6] == "tests/" and file_extension in ['.h', '.c', '.cpp']:
-			unit_test_files.add(_path)
-			file = open(_path, "r")
-			for line in file:
-				if re.search("BOOST_.*_TEST_CASE", line):
-					number_of_test_cases += 1
-					# print( line )
-				if re.search("BOOST_CHECK|BOOST_REQUIRE", line):
-					number_of_test_assertions += 1
-					# print( line )
+        # If it's a C++ unit test, then count the test cases and test points
+        if _path[0:6] == "tests/" and file_extension in ['.h', '.c', '.cpp']:
+            unit_test_files.add(_path)
+            file = open(_path, "r")
+            for line in file:
+                if re.search("BOOST_.*_TEST_CASE", line):
+                    number_of_test_cases += 1
+                # print( line )
+                if re.search("BOOST_CHECK|BOOST_REQUIRE", line):
+                    number_of_test_assertions += 1
+                # print( line )
 
-		elif "fileTemplates" in _path:
-			other_files.add(_path)
-		elif file_extension == '.h':
-			h_src_files.add(_path)
-		elif file_extension == '.c':
-			c_src_files.add(_path)
-		elif file_extension == '.cpp':
-			cpp_src_files.add(_path)
-		elif file_extension == '.py':
-			py_files.add(_path)
-		elif file_extension in ['.html', '.css']:
-			web_files.add(_path)
-		elif file_extension == '.asm':
-			asm_files.add(_path)
-		elif file_extension == '.md':
-			markdown_files.add(_path)
-		elif file_extension in ['.svg', '.png', '.ico', '.png', '.jpg', '.jpeg', '.dot']:
-			image_files.add(_path)
-		else:
-			other_files.add(_path)
+        elif "fileTemplates" in _path:
+            other_files.add(_path)
+        elif file_extension == '.h':
+            h_src_files.add(_path)
+        elif file_extension == '.c':
+            c_src_files.add(_path)
+        elif file_extension == '.cpp':
+            cpp_src_files.add(_path)
+        elif file_extension == '.py':
+            py_files.add(_path)
+        elif file_extension in ['.html', '.css']:
+            web_files.add(_path)
+        elif file_extension == '.asm':
+            asm_files.add(_path)
+        elif file_extension == '.md':
+            markdown_files.add(_path)
+        elif file_extension in ['.svg', '.png', '.ico', '.png', '.jpg', '.jpeg', '.dot']:
+            image_files.add(_path)
+        else:
+            other_files.add(_path)
 
-	process_gcc_files(".h Source", h_src_files)
-	process_gcc_files(".c Source", c_src_files)
-	process_gcc_files(".cpp Source", cpp_src_files)
-	process_gcc_files("Unit Tests", unit_test_files)
-	process_script_files("Python", py_files)
-	process_text_files("HTML", web_files)
-	process_text_files("Assembly", asm_files)
-	process_text_files("Markdown", markdown_files)
-	process_data_files("Images", image_files)
-	process_data_files("Other	Files", other_files)
+    process_gcc_files(".h Source", h_src_files)
+    process_gcc_files(".c Source", c_src_files)
+    process_gcc_files(".cpp Source", cpp_src_files)
+    process_gcc_files("Unit Tests", unit_test_files)
+    process_script_files("Python", py_files)
+    process_text_files("HTML", web_files)
+    process_text_files("Assembly", asm_files)
+    process_text_files("Markdown", markdown_files)
+    process_data_files("Images", image_files)
+    process_data_files("Other	Files", other_files)
 
 
 ## Print the number of commits to the repo
 ##
 ## @return Nothing
 def print_number_of_commits():
-	print("")
-	number_of_commits = len(list(repo.iter_commits('HEAD')))
-	print(f"Number of commits:  {number_of_commits}")
+    print("")
+    number_of_commits = len(list(repo.iter_commits('HEAD')))
+    print(f"Number of commits:  {number_of_commits}")
 
 
 ## Print the statistics related to Boost Tests in the repository
@@ -376,35 +385,35 @@ def print_number_of_commits():
 ##
 ## @return Nothing
 def print_test_stats():
-	global number_of_test_cases
-	global number_of_test_assertions
+    global number_of_test_cases
+    global number_of_test_assertions
 
-	if number_of_test_cases == 0:
-		return
+    if number_of_test_cases == 0:
+        return
 
-	print("")
-	print(f"Number of test cases:  {number_of_test_cases}")
-	print("")
-	print(f"Number of test assertions:  {number_of_test_assertions}")
+    print("")
+    print(f"Number of test cases:  {number_of_test_cases}")
+    print("")
+    print(f"Number of test assertions:  {number_of_test_assertions}")
 
 
 # The	Main Program
 
-with open(OUTPUT_FILE,	'w') as redirected_output:
-	## Redirect `stdout`
-	sys.stdout = redirected_output
-	print_title()
+with open(OUTPUT_FILE, 'w') as redirected_output:
+    ## Redirect `stdout`
+    sys.stdout = redirected_output
+    print_title()
 
-	process_files_in_git()
+    process_files_in_git()
 
-	print_tags()
+    print_tags()
 
-	print("## Software Engineering Metrics")
+    print("## Software Engineering Metrics")
 
-	print_number_of_commits()
+    print_number_of_commits()
 
-	print_test_stats()
+    print_test_stats()
 
-	print_shields_io_tags()
+    print_shields_io_tags()
 
-	print_date()
+    print_date()
--- STATISTICS.md
+++ STATISTICS.md
@@ -8,18 +8,18 @@ Project Statistics
 |------|------:|------:|------:|--------------:|--------------:|
 |.cpp Source|18|1240|40083|663|22952|
 |Unit Tests|1|81|2209|36|1086|
-|Python|3|595|17140|268|8791|
+|Python|3|604|17735|278|9303|
 |HTML|1|59|1420|n/a|n/a|
-|Markdown|9|1284|66422|n/a|n/a|
+|Markdown|10|1284|66422|n/a|n/a|
 |Images|20|n/a|2256825|n/a|n/a|
-|Other	Files|35|n/a|245274|n/a|n/a|
+|Other	Files|35|n/a|246216|n/a|n/a|
 
 ## Tags
 No tags
 
 ## Software Engineering Metrics
 
-Number of commits:  149
+Number of commits:  150
 
 Number of test cases:  1
 
@@ -28,6 +28,6 @@ Number of test assertions:  7
 ## GitHub Statistics
 | Repository                           | Commits                   | Issues                  | Social                    |
 |--------------------------------------|---------------------------|-------------------------|---------------------------|
-| ![GitHub code size	in	bytes](https://img.shields.io/github/languages/code-size/marknelsonengineer/empire?style=social) <br/> ![GitHub repo size](https://img.shields.io/github/repo-size/marknelsonengineer/empire?style=social) <br/> ![GitHub contributors](https://img.shields.io/github/contributors/marknelsonengineer/empire?style=social) | ![GitHub commit activity](https://img.shields.io/github/commit-activity/w/marknelsonengineer/empire?style=social) <br/> ![GitHub last	commit](https://img.shields.io/github/last-commit/marknelsonengineer/empire?style=social) | ![GitHub	issues](https://img.shields.io/github/issues-raw/marknelsonengineer/empire?style=social) <br/> ![GitHub	closed issues](https://img.shields.io/github/issues-closed-raw/marknelsonengineer/empire?style=social) | ![GitHub forks](https://img.shields.io/github/forks/marknelsonengineer/empire?style=social) <br/> ![GitHub Repo	stars](https://img.shields.io/github/stars/marknelsonengineer/empire?style=social) |
+| ![X](https://img.shields.io/github/languages/code-size/marknelsonengineer/empire?style=plastic) <br/> ![X](https://img.shields.io/github/repo-size/marknelsonengineer/empire?style=plastic) <br/> ![X](https://img.shields.io/github/contributors/marknelsonengineer/empire?style=plastic) | ![X](https://img.shields.io/github/commit-activity/w/marknelsonengineer/empire?style=plastic) <br/> ![X](https://img.shields.io/github/last-commit/marknelsonengineer/empire?style=plastic) | ![X](https://img.shields.io/github/issues-raw/marknelsonengineer/empire?style=plastic) <br/> ![X](https://img.shields.io/github/issues-closed-raw/marknelsonengineer/empire?style=plastic) | ![X](https://img.shields.io/github/forks/marknelsonengineer/empire?style=plastic) <br/> ![X](https://img.shields.io/github/stars/marknelsonengineer/empire?style=plastic) |
 
-Automatically generated on Wed May 17 13:58:29 2023
+Automatically generated on Wed May 17 14:31:42 2023
