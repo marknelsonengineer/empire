841f53cfa4da6edb0bde3f7fefe57b13798985a8
Mark Nelson <marknels@hawaii.edu>
2023-06-17 08:32:53 -1000

    Zero out the old logger code

--- src/lib/Log.cpp
+++ src/lib/Log.cpp
@@ -10,69 +10,8 @@
 /// @copyright (c) 2021 Mark Nelson.  All rights reserved.
 ///////////////////////////////////////////////////////////////////////////////
 
-
-/// Document this here TODO
-#define LOG_CHANNEL "Log"  /// @NOLINT( cppcoreguidelines-macro-usage ): A #define is OK here
 #include "Log.hpp"
 
-// Additional Boost Log #includes
-
 namespace empire {
-
-
-/// Define an override for the << operator that will map severity_level to a
-/// text description.
-///
-/// @internal This list must match (in the same order) as
-///           the severity_level enum.
-///
-/// @param strm  Stream @todo
-/// @param level Log level @todo
-/// @return Not sure, but we'll sort it out @todo
-//std::ostream& operator<< (std::ostream& strm, severity_level level) {
-//	static const char* strings[] = {
-//      "test"
-//	  ,"trace"
-//	  ,"debug"
-//	  ,"info"
-//	  ,"warning"
-//	  ,"error"
-//	  ,"critical"
-//	  ,"fatal"
-//    };
-//
-//    if (static_cast< std::size_t >(level) < sizeof(strings) / sizeof(*strings))
-//        strm << strings[level];
-//    else
-//        strm << static_cast< int >(level);
-//
-//    return strm;
-//}
-
-
-// Define the attribute keywords
-//BOOST_LOG_ATTRIBUTE_KEYWORD(severity ,"Severity", severity_level)
-//BOOST_LOG_ATTRIBUTE_KEYWORD(channel  ,"Channel", std::string)
-
-
-/// Initialize the global logger.
-///
-///   - Add a file logger
-///   - Add a console logger
-///   - Initialize the default logging level
-///
-/// The Channel is the filename of the source of the log, which is a good proxy
-/// for a channel.
-///
-/// LOG_CHANNEL should be set by -DLOG_CHANNEL=\"$*\" in the Makefile
-///
-/// @todo Create a custom log sink for testing log messages (consider unbounded_fifo_queue)
-/// @todo Implement a log-level API to change the log level in realtime
-///
-//BOOST_LOG_GLOBAL_LOGGER_INIT(empireLogger, logger_t) {
-//
-//	return logger;
-//}
-
 
 } // namespace empire
--- src/lib/Log.hpp
+++ src/lib/Log.hpp
@@ -14,44 +14,6 @@
 
 namespace empire {
 
-/// Severity levels
-///
-/// This list is in order of severity (from lowest to highest).
-enum severity_level {
-   test,     ///< Use for Boost Unit Tests
-   trace,    ///< Use when trying follow the thread of execution through code
-	debug,    ///< Information that is diagnostically helpful
-	info,     ///< Generally useful information
-	warning,  ///< Anything that can potentially cause application oddities
-	error,    ///< Any error which is fatal to an **operation**
-	fatal     ///< Any error which is fatal to the **process**
-};
-
-
-/// Define a default log severity level.  Logs at or above
-/// this level will be printed.  Logs below this level
-/// will be suppressed.
-///
-/// This default severity level is the same for both console
-/// and file-based logging.  Furthermore, it's the same for
-/// all channels.
-constinit const severity_level default_log_severity_level = test;
-
-
-/// LOG_CHANNEL should be set by each user of Log before the `#include`
-#ifndef LOG_CHANNEL
-   #pragma GCC warning "LOG_CHANNEL should be set by each user of Log before the #include"
-   #define LOG_CHANNEL "Unknown"
-#endif
-
-/// Define the Log sinks...
-///
-/// ## Usage
-///     LOG_INFO << "Starting " << PROGRAM_TITLE << " core services";
-///
-
-/// @todo Identify when we are doing a Release build and zero out LOG_TEST, LOG_TRACE and LOG_DEBUG
-
 /// Use for Boost Unit Tests
 #define LOG_TEST     (void)
 
